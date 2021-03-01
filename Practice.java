5cd6f5d855b871d023169f4cd567613ad66c34d6
irctcrailbookings <smsubhashp@hotmail.com>
2021-03-01 22:01:35 +0530

    930Classes:First commit

---
+++ Practice.java
@@ -0,0 +1,17 @@
+ /*This is a simple Java program. 
+   FileName : "HelloWorld.java". 
+   subhash
+   kumar 
+   pandey*/
+   
+ class HelloWord { 
+
+    //Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String[] args) { 
+		int a123Value =100;
+		int a123value =100;
+        System.out.println("Hello, World");
+		System.out.println(a123value);
+    } 
+} 
